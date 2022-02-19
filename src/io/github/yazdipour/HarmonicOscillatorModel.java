0211f79f2b4be33bdb57af5dcc05455f77a02914
Shahriar Yazdipour <shahriar.yazdipour@outlook.com>
2022-02-19 22:43:54 +0100

    Task#2

--- src/io/github/yazdipour/HarmonicOscillatorModel.java
+++ src/io/github/yazdipour/HarmonicOscillatorModel.java
@@ -1,5 +1,9 @@
 package io.github.yazdipour;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
 public class HarmonicOscillatorModel implements ResonanceModel {
     public HarmonicOscillatorParameters parameters;
 
@@ -21,5 +25,34 @@ public class HarmonicOscillatorModel implements ResonanceModel {
         if (denominator == 0.)
             throw new IllegalArgumentException("Dividing by zero.");
         return Math.pow(parameters.getAmplitude(), 2) / denominator;
+    }
+
+
+    /**
+     * Q2. Provide a method to write data from a HarmonicOscillatorModel instance to a file.
+     * Output should be in column format, e.g.
+     * # Frequency Spectral_Power
+     * 100.0 1.23e-12
+     * 200.0 1.81e-10
+     * 300.0 6.14e-10
+     * [...]
+     * 10000.0 7.11e-17
+     * <p>
+     * Calculate harmonic oscillator and write to file
+     *
+     * @param fileName      output filename
+     * @param startFreq     starting frequency
+     * @param stopFreq      stopping frequency
+     * @param stepFreq      step frequency
+     */
+    public void writeToFile(String fileName, double startFreq, double stopFreq, double stepFreq) throws IOException {
+        PrintWriter writer = new PrintWriter(fileName, StandardCharsets.UTF_8);
+        writer.println("# Frequency\tSpectral_Power");
+        for (double freq = startFreq; freq <= stopFreq; freq += stepFreq) {
+            double spectralPower = evaluateSpectralPowerAt(startFreq, parameters);
+            writer.printf("%7.1f\t\t%1.2e\n", freq, spectralPower);
+        }
+        writer.close();
+        System.out.printf("Output file: %s\n", fileName);
     }
 }
