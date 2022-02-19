7d8f35fa278579f738aec5616c77a468e48bf07c
Shahriar Yazdipour <shahriar.yazdipour@outlook.com>
2022-02-19 22:47:07 +0100

    Task#3

---
+++ src/io/github/yazdipour/Main.java
@@ -0,0 +1,39 @@
+package io.github.yazdipour;
+
+import java.io.IOException;
+
+public class Main {
+
+    /**
+     * Q3. Create a command line application that receives the parameters for a given resonance
+     * model on the command line and calls the method from step 2 to generate an output file.
+     * @param args input from user: amplitude omega0 qfactor filename
+     */
+    public static void main(String[] args) {
+        if (args.length < 4) {
+            System.err.println("Err, Please enter: amplitude omega0 qfactor filename");
+            System.exit(1);
+        }
+        double amplitude = 0;
+        double omega0 = 0;
+        double qFactor = 0;
+        try {
+            amplitude = Double.parseDouble(args[0]);
+            omega0 = Double.parseDouble(args[1]);
+            qFactor = Double.parseDouble(args[2]);
+        } catch (NumberFormatException e) {
+            System.err.println("Err, Please enter: amplitude omega q-factor properly");
+            System.exit(1);
+        }
+        String filename = args[3];
+        try {
+            HarmonicOscillatorParameters parameters = new HarmonicOscillatorParameters(amplitude, omega0, qFactor);
+            HarmonicOscillatorModel oscillator = new HarmonicOscillatorModel(parameters);
+            oscillator.writeToFile(filename, 100, 1000, 100);
+        } catch (FitFailedException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException ex) {
+            System.err.println("Err, Could not write to file");
+        }
+    }
+}
